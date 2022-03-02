ccca76c0eba579c718641949f260599db32c8141
viktor.patuta <viktor.patuta@extrawest.com>
2022-03-02 11:57:03 +0200

    Mobile gestures test case on Hybrid application

--- src/ecommerce_tc_4.java
+++ src/ecommerce_tc_4.java
@@ -1,10 +1,17 @@
 import java.net.MalformedURLException;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import static io.appium.java_client.touch.TapOptions.tapOptions;
+import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
+import static io.appium.java_client.touch.offset.ElementOption.element;
+import static java.time.Duration.ofSeconds;
 
+
+import io.appium.java_client.TouchAction;
 //import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
@@ -13,7 +20,7 @@ public class ecommerce_tc_4 extends base {
 
 	public static void main(String[] args) throws MalformedURLException, InterruptedException {
 
-		AndroidDriver<AndroidElement> driver = capabilities("real");
+		AndroidDriver<AndroidElement> driver = capabilities("emulator");
 
 		driver.findElementById("com.androidsample.generalstore:id/nameField").sendKeys("Hello");
 		driver.hideKeyboard();
@@ -62,7 +69,20 @@ public class ecommerce_tc_4 extends base {
 		System.out.println("Total value of products: " + totalValue);
 		
 		Assert.assertEquals(totalValue, sum);
-
+		
+		//Mobile gestures
+		
+		WebElement checkbox = driver.findElement(By.className("android.widget.CheckBox"));
+		
+		TouchAction<?> t = new TouchAction<>(driver);
+		t.tap(tapOptions().withElement(element(checkbox))).perform();
+		
+		WebElement tc = driver.findElement(By.xpath("//*[@text='Please read our terms of conditions']"));
+		
+		t.longPress(longPressOptions().withElement(element(tc)).withDuration(ofSeconds(2))).release().perform();
+		driver.findElement(By.id("android:id/button1")).click();
+		driver.findElement(By.id("com.androidsample.generalstore:id/btnProceed")).click();
+		
 	}
 	
 	public static double getAmount(String value) {
